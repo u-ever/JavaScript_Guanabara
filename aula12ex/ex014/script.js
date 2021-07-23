a28e23c601e9531453551ab9ac384dcf7273091a
u-ever <uever.martins@gmail.com>
2021-07-22 22:13:45 -0300

    Hora do Dia

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -6,15 +6,15 @@ function loading() {
     msg.innerText = `Agora são ${hora} horas.`
     if (hora <= 12) {
         //'Bom dia!'
-        img.innerHTML = '<img src="morning.jpg" alt="Foto">'
+        img.innerHTML = '<img src="pictures/morning.jpg" alt="Foto">'
         document.body.style.backgroundColor = '#e2cd9f'
     } else if (hora <= 18) {
        //'Boa tarde!'
-       img.innerHTML = '<img src="tarde1.jpg" alt="Foto">'
+       img.innerHTML = '<img src="pictures/afternoon.jpg" alt="Foto">'
        document.body.style.backgroundColor = '#b9846f'
     } else {
         //'Boa noite!'
-        img.innerHTML = '<img src="night.jpg" alt="Foto">'
+        img.innerHTML = '<img src="pictures/night.jpg" alt="Foto">'
         document.body.style.backgroundColor = '#515154'
     }
 }
