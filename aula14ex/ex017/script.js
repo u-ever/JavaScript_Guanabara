0e32705278e058aa9b478f233176bb8c722adb5f
u-ever <uever.martins@gmail.com>
2021-07-29 23:25:43 -0300

    tabuadajs

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,3 +1,33 @@
+function tabuada(){
+    let mult = document.getElementById('mult')
+    let res = document.getElementById('tab')
+
+    if (mult.value.length == 0) {
+        window.alert('Por favor, digite um número')
+    } else {
+        res.innerHTML = ``
+        let num = Number(mult.value)
+        let c = 1
+        for (c; c < 11; c++){
+            let numx = document.createElement('option')
+            numx.text = `${num} x ${c} = ${num * c}`
+            numx.value = `tab${c}`
+            tab.appendChild(numx)
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Mostrar uma taboada
 // window.alert com "Por favor divige um número!"
-
---
+++ aula14ex/ex017/testetabuada.py
@@ -0,0 +1,5 @@
+mult = int(input('Digite um número: '))
+
+for c in range (1,11):
+	print(f'{mult} x {c} = {mult * c}')
+	c += 1
