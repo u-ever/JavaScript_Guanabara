ec2b58e6147016d3952ce5d7a90a024cac56faee
u-ever <uever.martins@gmail.com>
2021-07-31 22:14:39 -0300

    ex16

---
+++ aula15/ambiente.js
@@ -0,0 +1,3 @@
+let num = [0,5,4,8,9,5,8]
+
+console.log(num.indexOf(8))
---
+++ ex016ex/script.js
@@ -0,0 +1,32 @@
+var lista = []
+
+function add() {
+    let num = document.getElementById('num')
+    if (num.length == 0) {
+        window.alert('Valor inválido ou já encontrado na lista')
+    } else {
+        
+    }
+
+}
+
+function analyze() {
+    if (lista.length == 0) {
+        window.alert('Adicione valores antes de finalizar!')
+    }
+}
+
+
+
+
+
+
+
+//OK Adicionar elementos básicos no HTML
+// Transferir dados digitados no imput para o select
+// Executar as seguintes validações:
+//- Ao clicar em adicionar com um valor repetido ou sem preencher o imput: //Valor inválido ou já encontrado na lista
+//- Ao clicar em finalizar sem nenhum dado na lista: Adicione valores antes de finalizar!
+// Contar os números cadastrados e exibi-los
+// Exibir o maior e o menor
+// Exibir a soma e a média
