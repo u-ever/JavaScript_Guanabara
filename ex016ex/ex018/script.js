89d0ac5702e0a102a2d57f9fca9faebe3fee195c
u-ever <uever.martins@gmail.com>
2021-08-01 23:55:17 -0300

    Project Complete version Uever

--- ex016ex/ex018/script.js
+++ ex016ex/ex018/script.js
@@ -2,10 +2,12 @@ let lista = document.querySelector('select#list')
 let num = document.querySelector('input#num')
 let res = document.querySelector('div#res')
 let vetores = []
-let c = 0
 
+/*verifica se um número foi digitado, se está no range de 0 a 100 e se já esta na lista, caso
+as condições sejam atendidas adiciona o valor digitado na input num a lista vetores e cria um 
+option na select list com o valor digitado. Após a inserção, deixa o input em branco*/
 function add() {
-    if (num.value.length == 0 || num.value > 100 || num.value < 0) {
+    if (num.value.length == 0 || num.value > 100 || num.value < 0 || verif(num.value) == true) {
         window.alert('Valor inválido ou já encontrado na lista')
     } else {
         let numadd = document.createElement('option')
@@ -15,43 +17,66 @@ function add() {
         vetores.push(num.value)
         document.querySelector('input#num').value = ""
     }
+    num.value = ''
 }
 
-num.value = ''
+//Verifica se o número digitado no input num já está no array vetores.
+function verif(n) {
+    if (vetores.indexOf(n) >= 0) {
+        return true
+    } else {
+        return false
+    }
+}
 
+/*verifica se existem números no array vetores, se positivo, realiza 5 verificações utilizando
+as funções count() max() min() sum() e media() */ 
 function analyze() {
     if (vetores.length == 0) {
         window.alert('Adicione valores antes de finalizar!')
     } else {
-        count()
-        maxMin()
+        res.innerHTML = `- Ao todo temos ${count()} números cadastrados<br>`
+        res.innerHTML += `- O maior valor digitado foi ${max()}<br>`
+        res.innerHTML += `- O menor valor digitado foi ${min()}<br>`
+        res.innerHTML += `- A soma dos valores foi ${sum()}<br>`  
+        res.innerHTML += `- A média dos valores foi ${media().toFixed(1)}`
     }
 }
 
+//conta quantos elementos existem no array vetores
 function count() {
-    for (c = 0; c <= vetores.length; c++) {
-        res.innerHTML = `Ao todo temos ${c} números cadastrados.<br>`
+    let cont = 0
+    for (let c = 0; c <= vetores.length; c++) {
+        cont += c
     }
+    return cont - 1
 }
 
-function maxMin() {
-    vetores.sort()
-    res.innerHTML += `O maior valor digitado foi ${vetores[vetores.length - 1]}.<br>`
-    res.innerHTML += `O menor valor digitado foi ${vetores[0]}.<br>`
+//Organiza os elementos do array vetores em ordem crescente e retorna o valor da ultima posição 
+function max() {
+    vetores.sort((a,b) => a-b)
+    return vetores[vetores.length - 1]
 }
 
+//Organiza os elementos do array vetores em ordem crescente e retorna o valor da primeira posição
+function min() {
+    vetores.sort((a,b) => a-b)
+    return vetores[0]
+}
+
+//Soma os valores do array vetores e retorna o resultado
 function sum() {
-    
+    let soma = 0
+    for (let s in vetores) {
+        soma += Number(vetores[s])
+    }
+    return soma
 }
 
-
-
-
-// Adicionar elementos básicos no HTML
-// Executar as seguintes validações:
-//- Ao clicar em adicionar com um valor repetido ou sem preencher o imput: //Valor inválido ou já encontrado na lista
-//- Ao clicar em finalizar sem nenhum dado na lista: Adicione valores antes de finalizar!
-// Transferir dados digitados no imput para o select
-// Contar os números cadastrados e exibi-los
-// Exibir o maior e o menor
-// Exibir a soma e a média
+/*Utiliza a função sum() para realizar uma divisão pelo numero total de elementos do 
+array vetores e retorna o resultado*/
+function media() {
+    let m = sum()
+    m /= Number(vetores.length)
+    return m
+}
